9a3620e6efbe24033d37677c222ab31907e4db22
ZidaruRobert <66086654+ZidaruRobert@users.noreply.github.com>
2020-11-09 19:37:05 +0200

    Updated v1.0.6

--- js/title.js
+++ js/title.js
@@ -31,7 +31,7 @@ var titleText = [ "|",
 					"Z I D A R U . |",
 					"Z I D A R U . X Y Z|",
 					"Z I D A R U  . X Y Z |",
-					"Z I D A R U . X Y Z |"];
+					"Z I D A R U . X Y Z|"];
 
 function loop(){
 
